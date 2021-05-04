2f7d2e00f65021060482efcd85298f3221e8a9b8
aneagoe <adrian.neagoe@1and1.ro>
2021-05-04 21:55:37 +0300

    add vue

---
+++ client/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{joke}}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      joke: Object
+    }
+  },
+  mounted: function (){
+    fetch('https://official-joke-api.appspot.com/random_joke', {
+      method: 'get'
+    })
+        .then((response) => {
+          return response.json()
+        })
+        .then((jsonData) => {
+          this.joke = jsonData
+        })
+  }
+}
+
+
+
+
+</script>
